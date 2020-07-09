1c684804d71167dda283c737fa9c22a333de0ca0
antonio <alopez7@uc.cl>
2020-07-09 15:17:13 -0400

    test(demo/index): add test for index for now empty

--- spec/requests/demo_request_spec.rb
+++ spec/requests/demo_request_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Demos", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/demo/index"
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+    it "home routes to DemoController#index" do
+      assert_recognizes({ controller: "demo", action: "index" }, { method: "get", path: "/" })
     end
   end
 end
