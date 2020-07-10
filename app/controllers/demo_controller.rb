c879346057a2fa8105c4b5d47dba3a5721c4f458
antonio <alopez7@uc.cl>
2020-07-10 15:39:20 -0400

    feat(ImageProcessorUtils): add utils for image processing

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,36 @@
 class DemoController < ApplicationController
+  GOOGLE_API_KEY = ENV.fetch("GOOGLE_API_KEY")
+
   def index
-    puts "called this method"
+    puts "I'm in the home page!"
+  end
+
+  private
+
+  def process
+    word_collection = Guillotine::WordCollection.build_from_url image_url, GOOGLE_API_KEY
+    stencil = DrivingLicenceStencil.match word_collection
+    @class = stencil.class
+    @number = stencil.number
+    @municipality = stencil.municipality
+    @names = stencil.names
+    @surnames = stencil.surnames
+    @adress = stencil.adress
+    @issue_date = stencil.issue_date
+    @expiration_date = stencil.expiration_date
+    @transform = stencil.match.transform
+    @error = stencil.match.error
+  end
+
+  def image_url
+    upload_image_url.presence || default_image_url
+  end
+
+  def upload_image_url
+    nil
+  end
+
+  def default_image_url
+    "https://upload.wikimedia.org/wikipedia/commons/f/fe/El_ejemplo_de_Cedula_identidad_Chile_2013.jpg"
   end
 end
---
+++ spec/utils/image_processor_utils_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "ImageProcessorUtils" do
+  pending "test this file"
+end
