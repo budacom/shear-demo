a0a87d4bbe1192825636f91d56c81c8630d6f61a
antonio <alopez7@uc.cl>
2020-07-13 15:55:03 -0400

    test(image_processing): added tests for the image processing utils module

--- app/utils/image_processing_utils.rb
+++ app/utils/image_processing_utils.rb
@@ -1,10 +1,16 @@
+require "mini_magick"
+
 module ImageProcessingUtils
   extend self
+
+  def image_from_url(_url)
+    MiniMagick::Image.open(_url).get_pixels
+  end
 
   def transformed_image(_image, _matrix, _scaling = 10, _target_width = 85, _target_height = 55)
     width = _target_width * _scaling
     height = _target_height * _scaling
-    new_image = array.new(height) { array.new(width) { array.new(3, 0) } }
+    new_image = Array.new(height) { Array.new(width) { Array.new(3, 0) } }
     transform = _matrix.inverse
     fill_new_image(new_image, transform, _scaling, _image)
     new_image
@@ -18,7 +24,7 @@ module ImageProcessingUtils
         position = target_position(_transform, x, y, _scaling)
 
         if inside?(position, _image)
-          target_pixel = _image[position[y]][position[x]]
+          target_pixel = _image[position[1]][position[0]]
           pixel[0] = target_pixel[0]
           pixel[1] = target_pixel[1]
           pixel[2] = target_pixel[2]
@@ -35,19 +41,19 @@ module ImageProcessingUtils
     x = _origin_x / _scaling
     y = _origin_y / _scaling
     position = _transform * Matrix.column_vector([x, y, 1.0])
-    [position[0].to_i, position[1].to_i]
+    [position[0, 0].to_i, position[1, 0].to_i]
   end
 
   def inside?(_position, _image)
     height = _image.length
     width = _image[0].length
-    return false if position[0].negative?
+    return false if _position[1].negative?
 
-    return false if position[0] >= height
+    return false if _position[1] >= height
 
-    return false if position[1].negative?
+    return false if _position[0].negative?
 
-    return false if position[1] >= width
+    return false if _position[0] >= width
 
     true
   end
