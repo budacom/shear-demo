09d18bf28b2544fddc0aea1efd80acaa69eac266
antonio <alopez7@uc.cl>
2020-08-10 15:02:38 -0400

    fix(application.js): used old application.js

---
+++ app/javascript/app.spec.js
@@ -0,0 +1,13 @@
+import App from 'app';
+
+describe('App', () => {
+  test('is a Vue instance', () => {
+    const wrapper = shallowMount(App);
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it('displays message on load', () => {
+    const wrapper = shallowMount(App);
+    expect(wrapper.find('p').text()).toEqual('Hello Vue!');
+  });
+});
